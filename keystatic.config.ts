eb0252327ff36b598015a306716f34c469c22513
Wahyu SA <27491616+wahyusa@users.noreply.github.com>
2026-01-05 08:54:05 +0000

    feat: experiment add content component "testimonial"
    It render insertable testimonial in keystatic admin UI | It saves to markdoc component in post .mdoc

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,19 +1,31 @@
 // keystatic.config.ts
-import { config, fields, collection } from '@keystatic/core';
+import { config, fields, collection } from "@keystatic/core";
+import { wrapper } from "@keystatic/core/content-components";
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: "local",
   },
   collections: {
     posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'src/content/posts/*',
-      format: { contentField: 'content' },
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/posts/*",
+      format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
-        content: fields.markdoc({ label: 'Content' }),
+        title: fields.slug({ name: { label: "Title" } }),
+        content: fields.markdoc({
+          label: "Content",
+          components: {
+            Testimonial: wrapper({
+              label: "Testimonial",
+              schema: {
+                author: fields.text({ label: "Author" }),
+                role: fields.text({ label: "Role" }),
+              },
+            }),
+          },
+        }),
       },
     }),
   },
