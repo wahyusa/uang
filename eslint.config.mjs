5fff866573e151ec48b44c30a28fd4573fcb0c61
Wahyu SA <27491616+wahyusa@users.noreply.github.com>
2026-01-07 06:01:37 +0000

    refactor: Add new eslin configuration and rule for import
    - Now using import importPlugin from "eslint-plugin-import"; especially for src/components

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+import importPlugin from "eslint-plugin-import";
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
@@ -13,6 +15,37 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      // Ban default exports by default
+      "import/no-default-export": "error",
+
+      // Ensure named imports match named exports
+      "import/named": "error",
+    },
+  },
+  // Exceptions — files that MUST allow default export
+  {
+    files: [
+      // Next.js App Router
+      "src/app/**/{page,layout,template,not-found,loading,error,global-error}.tsx",
+      "src/app/**/route.ts",
+
+      // Tooling / configs
+      "keystatic.config.*",
+      "tailwind.config.*",
+      "next.config.*",
+      "eslint.config.*",
+      "postcss.config.*",
+    ],
+    rules: {
+      "import/no-default-export": "off",
+      "import/prefer-default-export": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
